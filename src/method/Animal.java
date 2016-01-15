3b020a58767e2ce22ed175f6478df03c53ece6cc
GrizZKA <kolendo.dm@gmail.com>
2016-01-15 17:22:18 +0200

    method1

---
+++ src/method/Animal.java
@@ -0,0 +1,33 @@
+package method;
+
+
+public class Animal{
+    String name;
+    int age;
+    int weight;
+    int power;
+    String color;
+
+    Animal animal1 = new Animal();
+    animal1.setName("Tiger");
+    animal1.setAge
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    public void setPower(int power){
+        this.power = power;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+}
